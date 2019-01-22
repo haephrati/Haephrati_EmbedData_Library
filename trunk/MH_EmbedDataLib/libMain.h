01088d44a056bb3d58024199ef05b02bfbb73ebc
securedglobe <securedglobe@b524786b-8245-41de-bf8a-8b613fa7e052>
2019-01-22 14:10:47 +0000

    First commit

---
+++ trunk/MH_EmbedDataLib/libMain.h
@@ -0,0 +1,14 @@
+//
+//	Embed resource in a Static Library - library header file
+//	by Michael Haephrati https://www.codeproject.com/Members/Michael-Haephrati
+//	Date :		30 March,2017
+//	Updated :	22 January, 2019
+//
+
+#include <Windows.h>
+#include <tchar.h>
+
+
+HICON GetIcon();
+void DisplayMessageWithIcon(LPWSTR message);
+
